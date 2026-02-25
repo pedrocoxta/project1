066edce61918e818b71c786965a3b217da38bf29
Pedro Costa <pedroguergolet1@gmail.com>
2026-02-24 23:06:15 -0300

    testando

--- teste.py
+++ teste.py
@@ -1,1 +1,8 @@
-print('oi teste')
+print('oi teste') #Mostrar mensagem na tela "Oi teste"
+if 5 > 3 : #Verificar se 5 é maior que 3
+    print('5 é maior que 3, óbvio!') # Mostrar mensagem na tela "5 é maior que 3"  
+    if 5 < 3 : #Verificar se 5 é menor que 3
+        print('5 é menor que 3, impossível!') # Mostrar mensagem na tela "5 é menor que 3"
+    else : #Caso 5 seja igual a 3
+        print('5 é igual a 3, não acredito!') # Mostrar mensagem na tela "5 é igual a 3"
+
